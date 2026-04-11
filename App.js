667a3de1fd7a293756862e096d1f82151484f8e7
Jacob Almeda <jacob.almeda0@gmail.com>
2026-04-11 17:31:55 -0400

    update documentation

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 /*
-Node + Express Example code for CS160 Summer 2022
-Prepared by Shm Garanganao Almeda 
+Node + Express website for MUS370-04: Music: Joy, Creativity, & Play and for the Joy Symposium during TCNJ's Spring 2026 semester
+Made by Jacob Almeda
+
+Template prepared by Shm Garanganao Almeda for CS160 Summer 2022
 
 Code referenced from: 
 https://www.digitalocean.com/community/tutorials/how-to-create-a-web-server-in-node-js-with-the-http-module"
@@ -27,7 +29,6 @@ var publicPath = path.join(__dirname, 'public'); //get the path to use our "publ
 app.use(express.static(publicPath));  //tell express to use that folder
 
 
-
 //here's where we specify what to send to users that connect to our web server...
 //if there's no url extension, it will show "index.html"
 router.get("/", function (req, res) {
@@ -50,6 +51,3 @@ app.get('/c', function (req, res) {
    app.listen(port, () => {
      console.log(`Server is running on http://${host}:${port}`);
    });
-
-
-
